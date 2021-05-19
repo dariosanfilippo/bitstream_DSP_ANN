ff78ad97c8c05bb3b824a752ee4f669a141388a8
Dario Sanfilippo <sanfilippo.dario@gmail.com>
2021-05-19 12:32:07 +0200

    testing gen

--- data.c
+++ data.c
@@ -1,9 +1,10 @@
-void gen_freq(Sig* in) {
+void gen_freq(Sig* in, Sig* out) {
     double freq = 0;
     for (size_t i = 0; i < in->space_dim; i++) {
-        freq = (rand() / (double) RAND_MAX) * 1000;
+        freq = (rand() / (double) RAND_MAX) * 800 + 200;
         sine(192000, .5, freq, in->vec_space[i], in->vec_len);
         dsm3(in->vec_space[i], in->vec_space[i], in->vec_len);
+        out->vec_space[i][0] = freq;
     }
 }
 
--- data_test.c
+++ data_test.c
@@ -8,12 +8,20 @@
 #include "data.c"
 
 int main(void) {
+    
     Sig* in = malloc(sizeof(Sig));
+    Sig* out = malloc(sizeof(Sig));
     sig_alloc(in, 4096, 1024);
-    gen_freq(in);
+    sig_alloc(out, 4096, 1);
+    
+    gen_freq(in, out);
 
     sig_print(in, 434);
     sig_print(in, 103);
+    
+    for (size_t i = 0; i < out->space_dim; i++) {
+        sig_print(out, i);
+    }
 
     return 0;
 }
