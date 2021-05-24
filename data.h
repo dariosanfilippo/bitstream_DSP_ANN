2a777c23f83aec1fec5184dcbfacea07657b7eb7
Dario Sanfilippo <sanfilippo.dario@gmail.com>
2021-05-24 19:19:06 +0200

    add data modules and flexible float/double type for audio signals

---
+++ data.h
@@ -0,0 +1,9 @@
+#ifndef DATA
+#define DATA
+
+void freq_est_data(Sig* in, Sig* out);
+void amp_est_data(Sig* in, Sig* out);
+void add_data(Sig* in0, Sig* in1, Sig* out);
+void mul_data(Sig* in0, Sig* in1, Sig* out);
+
+#endif
--- signals.h
+++ signals.h
@@ -1,6 +1,14 @@
 #ifndef SIGNALS
 #define SIGNALS
 
+#if audio == 32
+# define audio float
+#elif audio == 64
+# define audio double
+#else
+#error invalid audio size: it must be 32 or 64 bits
+#endif
+
 typedef struct Sig Sig;
 
 void sig_alloc(Sig* sig, size_t dim, size_t len);
