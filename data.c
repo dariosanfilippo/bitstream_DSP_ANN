51f31f13b84b6bbe59567ada3b303d8b4f12f0e2
Dario Sanfilippo <sanfilippo.dario@gmail.com>
2021-05-26 21:38:40 +0200

    added mul data generator and freed sigs where missing

--- data.c
+++ data.c
@@ -68,18 +68,47 @@ void adder_data(Sig* in, Sig* out) {
             in->vec_space[i][j + in1->vec_len] = in1->vec_space[i][j];
         }
     }
+
+    sig_free(in0);
+    sig_free(in1);
 }
 
+void mul_data(Sig* in, Sig* out) {
+    assert(in->space_dim == out->space_dim);
 
+    audio freq0;
+    audio freq1;
+    audio amp0;
+    audio amp1;
+    audio phase0;
+    audio phase1;
+    Sig* in0 = malloc(sizeof(Sig));
+    Sig* in1 = malloc(sizeof(Sig));
+    sig_alloc(in0, in->space_dim, in->vec_len / 2, in->sr);
+    sig_alloc(in1, in->space_dim, in->vec_len / 2, in->sr);
 
+    for (size_t i = 0; i < in->space_dim; i++) {
+        freq0 = (random() / (audio) RAND_MAX) * 5000.0;
+        freq1 = (random() / (audio) RAND_MAX) * 5000.0;
+        amp0 = (random() / (audio) RAND_MAX) * .5;
+        amp1 = (random() / (audio) RAND_MAX) * .5;
+        phase0 = (random() / (audio) RAND_MAX) * TWOPI;
+        phase1 = (random() / (audio) RAND_MAX) * TWOPI;
+        sine(in0->sr, amp0, freq0, phase0, in0, i);
+        sine(in1->sr, amp1, freq1, phase1, in1, i);
+        for (size_t j = 0; j < out->vec_len; j++) {
+            out->vec_space[i][j] = in0->vec_space[i][j] * in1->vec_space[i][j];
+        }
+        dsm3(in0, in0, i, i);
+        dsm3(in1, in1, i, i);
+        dsm3(out, out, i, i);
+        for (size_t j = 0; j < in0->vec_len; j++) {
+            in->vec_space[i][j] = in0->vec_space[i][j];
+            in->vec_space[i][j + in1->vec_len] = in1->vec_space[i][j];
+        }
+    }
 
-
-
-
-
-
-
-
-
-
+    sig_free(in0);
+    sig_free(in1);
+}
 
