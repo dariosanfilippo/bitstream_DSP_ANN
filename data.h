16379f7f62b8ba35cd55228ffea43df7f7866455
Dario Sanfilippo <sanfilippo.dario@gmail.com>
2021-05-26 21:41:41 +0200

    fixed data header

--- data.h
+++ data.h
@@ -3,7 +3,7 @@
 
 void freq_est_data(Sig* in, Sig* out);
 void amp_est_data(Sig* in, Sig* out);
-void add_data(Sig* in0, Sig* in1, Sig* out);
-void mul_data(Sig* in0, Sig* in1, Sig* out);
+void adder_data(Sig* in, Sig* out);
+void mul_data(Sig* in, Sig* out);
 
 #endif
